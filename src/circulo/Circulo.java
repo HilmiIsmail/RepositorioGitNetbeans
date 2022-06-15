a2583b9e20d5d76dc4912c58b49a0456350e516f
Ismail Hilmi <676ism@gmail.com>
2022-06-15 13:28:12 +0200

    Commit

--- src/circulo/Circulo.java
+++ src/circulo/Circulo.java
@@ -18,11 +18,16 @@ public class Circulo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        try{
         Scanner s=new Scanner(System.in);
         System.out.println("Introduce el radio:");
         double r=s.nextDouble();
         CalculaArea(r);
+        }
+        catch(Exception e){
+            System.out.printf(e.getMessage());
+        }
+        
         
     }
     public static void CalculaArea(double r) {
